d10a74bf7c36c9b952eaa2ed3685cf48e8d97015
Bernhard Reitinger <br@rexos.org>
2020-04-14 13:18:46 +0200

    Move to subfolder

--- code/REX/RexExporter.h
+++ code/REX/RexExporter.h
@@ -48,6 +48,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <assimp/types.h>
 #include <sstream>
 #include <vector>
+#include <memory>
 #include <map>
 
 #ifdef _WIN32
