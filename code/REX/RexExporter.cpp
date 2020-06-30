cfb19ab8f4b1709d377ce2fe70bd657739dbe58d
Bernhard Reitinger <br@rexos.org>
2020-06-30 11:21:36 +0200

    Fix win32 build break

--- code/REX/RexExporter.cpp
+++ code/REX/RexExporter.cpp
@@ -204,8 +204,6 @@ void RexExporter::WriteImages(rex_header *header, uint64_t startId, std::vector<
         i++;
     }
     imagePtrs.resize(m_TextureMap.size() - nrOfNotFoundImages);
-
-    printf("Texture files: assigned: %zu, loaded: %lu\n", m_TextureMap.size(), m_TextureMap.size() - nrOfNotFoundImages);
 }
 
 // ------------------------------------------------------------------------------------------------
@@ -267,9 +265,9 @@ void RexExporter::WritePoints(uint64_t startId, rex_header *header, DataPtr &poi
     std::vector<float> colorArray;
     GetColorArray(colors, colorArray);
 
-    rexPointList.nr_vertices = m_Points.size();
+    rexPointList.nr_vertices = (uint32_t)m_Points.size();
     if (m_Points.size() > 0 && m_Points[0].hasColor) {
-        rexPointList.nr_colors = m_Points.size();
+        rexPointList.nr_colors = (uint32_t)m_Points.size();
         rexPointList.colors = &colorArray[0];
     } else {
         rexPointList.nr_colors = 0;
